b011aefa2e8cf9f7b9e2f27d453579a234b89c5e
Eli Kamin <eli.kamin@hpe.com>
2021-07-22 11:46:36 -0500

    CRAYSAT-1032, CRAYSAT-1030: Rewrite install utility code
    This commit rewrites most of the sat_install_utility code to accomplish
    several goals.
    * Fix CRAYSAT-1032: Do not allow Docker images to be removed unless they
      are not being used by any other installed product.
    * Fix CRAYSAT-1030: Only allow removal of products that are listed in
      the product catalog.
    * Simplify code as much as possible by removing ExtendedNexusAPI and
      ExtendedDockerAPI classes.
    * Improve error handling.
    * Allow the ability to override Nexus package repository and registry
      URLs.
    * Improve unit test coverage.
    
    Test Description: After pushing this branch, I ran the resulting
    container image with Kubernetes configuration and SSL certificates
    cross-mounted into the container to activate and uninstall different
    versions of SAT on Thanos.

--- sat_install_utility/main.py
+++ sat_install_utility/main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 """
 Entry point for the SAT install utility.
 
@@ -24,9 +23,37 @@ OTHER DEALINGS IN THE SOFTWARE.
 """
 
 import argparse
+import warnings
 
-from sat_install_utility.nexus import ExtendedNexusAPI, ExtendedDockerAPI, NexusAPIError
-from sat_install_utility.product_version import InstalledProductVersion
+from kubernetes.config import load_kube_config, ConfigException
+from kubernetes.client import CoreV1Api
+from nexusctl import DockerApi, DockerClient, NexusApi, NexusClient
+from nexusctl.common import DEFAULT_DOCKER_REGISTRY_API_BASE_URL, DEFAULT_NEXUS_API_BASE_URL
+from yaml import YAMLLoadWarning
+
+from sat_install_utility.products import ProductCatalog, ProductInstallException
+
+PRODUCT_CATALOG_CONFIG_MAP_NAME = 'cray-product-catalog'
+PRODUCT_CATALOG_CONFIG_MAP_NAMESPACE = 'services'
+
+
+def get_k8s_api():
+    """Load a Kubernetes CoreV1Api and return it.
+
+    Returns:
+        CoreV1Api: The Kubernetes API.
+
+    Raises:
+        SystemExit: if there was an error loading the Kubernetes configuration.
+    """
+    try:
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore', category=YAMLLoadWarning)
+            load_kube_config()
+        return CoreV1Api()
+    except ConfigException as err:
+        print(f'Unable to load kubernetes configuration: {err}.')
+        raise SystemExit(1)
 
 
 def create_parser():
@@ -37,48 +64,103 @@ def create_parser():
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('action', choices=['uninstall', 'activate'])
-    parser.add_argument('product', choices=['sat'])
-    parser.add_argument('version')
+    parser.add_argument(
+        'action',
+        choices=['uninstall', 'activate'],
+        help='Specify the operation to execute on a product.'
+    )
+    parser.add_argument(
+        'product',
+        choices=['sat'],
+        help='Specify the name of the product to operate on.'
+    )
+    parser.add_argument(
+        'version',
+        help='Specify the version of the product to operate on.'
+    )
+    parser.add_argument(
+        '--dist',
+        default='sle-15sp2',
+        help='When manipulating group and hosted repositories, specify the '
+             'name of the distribution, which is assumed to be the tail of the'
+             'repositories\' names, e.g. sat-2.11-sle-15sp2.'
+    )
+    parser.add_argument(
+        '--nexus-url',
+        default=DEFAULT_NEXUS_API_BASE_URL,
+        help='Specify the base URL of Nexus.'
+    )
+    parser.add_argument(
+        '--docker-url',
+        default=DEFAULT_DOCKER_REGISTRY_API_BASE_URL,
+        help='Specify the base URL of the Docker registry.'
+    )
 
     return parser
 
 
-def uninstall(product, version):
+def uninstall(name, version, dist, nexus_api, docker_api):
     """Uninstall a version of a product.
 
     Args:
-        product: The name of the product, e.g. 'sat'
-        version: The version of the product stream, e.g. '2.1.11'
+        name (str): The name of the product, e.g. 'sat'
+        version (str): The version of the product, e.g. '2.1.11'
+        dist (str): Specify the distribution for the hosted repository to remove.
+        nexus_api (NexusApi): The nexusctl Nexus API to interface with
+            Nexus.
+        docker_api (DockerApi): The nexusctl Docker API to interface with
+            the Docker registry.
 
     Returns:
         None
+
+    Raises:
+        ProductInstallException: if uninstall failed.
     """
-    # TODO: override default nexus URL
-    product_to_uninstall = InstalledProductVersion(product, version)
-    product_to_uninstall.uninstall(ExtendedNexusAPI(), ExtendedDockerAPI())
+    product_catalog = ProductCatalog(
+        PRODUCT_CATALOG_CONFIG_MAP_NAME, PRODUCT_CATALOG_CONFIG_MAP_NAMESPACE, get_k8s_api()
+    )
+    product_to_uninstall, other_products = product_catalog.get_matching_products(name, version)
+
+    product_to_uninstall.uninstall_hosted_repo(nexus_api, dist)
+
+    # TODO(CRAYSAT-1033): Will have to be modified slightly for multiple Docker images per product
+    other_products_with_same_docker_image = [
+        product for product in other_products
+        if product.docker_image_name == product_to_uninstall.docker_image_name
+        and product.docker_image_version == product_to_uninstall.docker_image_version
+    ]
+    if other_products_with_same_docker_image:
+        print(f'Not removing Docker image '
+              f'{product_to_uninstall.docker_image_name}:{product_to_uninstall.docker_image_version} '
+              f'used by the following other product versions: '
+              f'{", ".join(str(p) for p in other_products_with_same_docker_image)}')
+    else:
+        product_to_uninstall.uninstall_docker_image(docker_api)
 
 
-def activate(product, version):
+def activate(name, version, dist, nexus_api):
     """Activate a version of a product.
 
     Args:
-        product: The name of the product, e.g. 'sat'
-        version: The version of the product stream, e.g. '2.1.11'
+        name (str): The name of the product, e.g. 'sat'
+        version (str): The version of the product, e.g. '2.1.11'
+        dist (str): The name of the distribution associated with the hosted
+            and group repositories.
+        nexus_api (NexusApi): The nexusctl Nexus API to interface with
+            Nexus.
 
     Returns:
         None
+
+    Raises:
+        ProductInstallException: If activation failed.
     """
-    product_to_activate = InstalledProductVersion(product, version)
-    try:
-        # TODO: override default nexus URL
-        product_to_activate.activate(ExtendedNexusAPI())
-    except NexusAPIError as err:
-        # TODO: handle exceptions from NexusApi (not just exceptions from ExtendedNexusAPI)
-        # e.g. we get urllib.error.URLError from requests if SSL connection failed.
-        # We should probably consolidate the error types and look at adding the extensions back into NexusApi
-        print(err)
-        raise SystemExit(1)
+    product_catalog = ProductCatalog(
+        PRODUCT_CATALOG_CONFIG_MAP_NAME, PRODUCT_CATALOG_CONFIG_MAP_NAMESPACE, get_k8s_api()
+    )
+    product_to_activate = product_catalog.get_matching_products(name, version)[0]
+    product_to_activate.activate_hosted_repo(nexus_api, dist)
 
 
 def main():
@@ -86,13 +168,25 @@ def main():
 
     Returns:
         None
+
+    Raises:
+        SystemExit: if a ProductInstallException occurs.
     """
     parser = create_parser()
     args = parser.parse_args()
-    if args.action == 'uninstall':
-        uninstall(args.product, args.version)
-    elif args.action == 'activate':
-        activate(args.product, args.version)
+    try:
+        if args.action == 'uninstall':
+            uninstall(
+                args.product, args.version, args.dist, NexusApi(NexusClient(args.nexus_url)),
+                DockerApi(DockerClient(args.docker_url))
+            )
+        elif args.action == 'activate':
+            activate(
+                args.product, args.version, args.dist, NexusApi(NexusClient(args.nexus_url))
+            )
+    except ProductInstallException as err:
+        print(err)
+        raise SystemExit(1)
 
 
 if __name__ == '__main__':
