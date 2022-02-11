8aa26aecc5e2eb2012c0c010ca1bda385748ae42
Ryan Haasken <haasken@cray.com>
2022-02-10 18:22:10 -0600

    CRAYSAT-1301: Use new `cfs-config-util` version
    Update `sat-install-utility` to use the newer 2.0.0 version of
    `cfs-config-util`. This version changes the arguments to
    `cfs_activate_version` to no longer take a layer name. Instead, it
    constructs the layer name from the product name and version.
    
    Remove unused import of `cfs_deactivate_version` from `cfs_config_util`
    since it is not currently being used, and we should put some careful
    thought into how we use it.
    
    Test Description:
    Tested by modifying `Dockerfile` to install the new 2.0.0 version of
    `cfs-config-util` from a local copy. Built docker image locally, saved
    it to a tar file with `docker save`. Copied that container image to
    lemondrop with `scp`. Loaded into podman on lemondrop with `podman
    load`. Manually modified the product catalog configmap to add a fake
    2.2.17 version which referenced this new sat-install-utility:1.4.3
    container image and created a corresponding cray/sat/2.2.17 branch in
    the sat-config-management repository which pointed at a new commit. Ran
    `prodmgr activate sat 2.2.17` and confirmed that the sat layer of the
    'ncn-personalization' cfs configuration was updated.
    
    Unit tests pass.
    
    (cherry picked from commit fd06221c8b261865c7b2e31af5e2cf95da0a0305)

--- sat_install_utility/constants.py
+++ sat_install_utility/constants.py
@@ -1,9 +1,8 @@
 """
 Constants used by sat-install-utility.
 
-(C) Copyright 2021 Hewlett Packard Enterprise Development LP.
+(C) Copyright 2021-2022 Hewlett Packard Enterprise Development LP.
 """
 
 PRODUCT = 'sat'
-PRODUCT_CFS_NCN_CFG_LAYER = f'{PRODUCT}-ncn'
 PRODUCT_NCN_PLAYBOOK = f'{PRODUCT}-ncn.yml'
